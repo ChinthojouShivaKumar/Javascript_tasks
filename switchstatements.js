ae2ea16287e145fb7149be3bad3bf6c4042ff5af
ChinthojouShivaKumar <124147339+ChinthojouShivaKumar@users.noreply.github.com>
2023-05-01 15:38:52 +0530

    Create switchstatements.js

---
+++ switchstatements.js
@@ -0,0 +1,43 @@
+var months = prompt("enter your month number");
+
+
+switch(parseInt(months)) {
+case 0 : 
+alert("Your input month is January")
+break;
+case 1 : 
+alert(" Your input month is Febrauary")
+break;
+case 2 : 
+alert(" Your input month is March")
+break;
+case 3 : 
+alert("Your input month is April")
+break;
+case 4 : 
+alert("Your input month is May")
+break;
+case 5 : 
+alert("Your input month is June")
+break;
+case 6 : 
+alert("Your input month is July")
+break;
+case 7 : 
+alert("Your input month is August")
+break;
+case 8 : 
+alert("Your input month is September")
+break;
+case 9 : 
+alert("Your input month is October")
+break;
+case 10 : 
+alert("Your input month is November")
+break;
+case 11 : 
+alert("Your input month is December")
+break;
+default :
+alert("Invalid input")
+}
