ca97bd438bf7b60a29e9904dff3f8b265aebda03
ChinthojouShivaKumar <124147339+ChinthojouShivaKumar@users.noreply.github.com>
2023-05-02 14:58:30 +0530

    Create function_evenodd.js

---
+++ function_evenodd.js
@@ -0,0 +1,31 @@
+function shiva(a,b) {
+    for (i=0;i<20;i++) {
+        if(i%2==0)
+       
+        console.log(i)
+    }
+}
+
+shiva()
+
+
+function mani(a,b) {
+    return a+b
+}
+
+console.log(mani(1,2))
+
+
+☁️ "Running fiddle"
+"Welcome to Programiz!"
+0
+2
+4
+6
+8
+10
+12
+14
+16
+18
+3
