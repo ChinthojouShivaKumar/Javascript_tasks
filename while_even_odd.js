9c3de4c738cddfeb41ff4231f4f1f241508114fc
ChinthojouShivaKumar <124147339+ChinthojouShivaKumar@users.noreply.github.com>
2023-05-02 14:51:43 +0530

    Create while_even_odd.js
    while loop even and odd

---
+++ while_even_odd.js
@@ -0,0 +1,17 @@
+var a = 0;
+var arr =[];
+do {
+    if (a%2!==0)
+    arr.push(a)
+    
+    a++
+    
+}while(a<30)
+
+console.log(arr)
+
+
+
+
+☁️ "Running fiddle"
+[1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29]
