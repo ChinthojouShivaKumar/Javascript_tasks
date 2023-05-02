ceedb18e24314578422cb12cc60450ea456a5411
ChinthojouShivaKumar <124147339+ChinthojouShivaKumar@users.noreply.github.com>
2023-05-02 11:35:37 +0530

    Create forloop.js

---
+++ forloop.js
@@ -0,0 +1,41 @@
+console.log("Welcome to Programiz!");
+
+
+
+var num = prompt("enter the range to print tha output?");
+for (a=0; a<num; a++) {
+console.log("Output is: " + a)
+}
+
+
+output:-
+
+
+"Welcome to Programiz!"
+"Output is: 0"
+"Output is: 1"
+"Output is: 2"
+"Output is: 3"
+☁️ "Running fiddle"
+"Welcome to Programiz!"
+"Output is: 0"
+"Output is: 1"
+"Output is: 2"
+"Output is: 3"
+"Output is: 4"
+"Output is: 5"
+"Output is: 6"
+"Output is: 7"
+☁️ "Running fiddle"
+"Welcome to Programiz!"
+"Output is: 0"
+"Output is: 1"
+"Output is: 2"
+"Output is: 3"
+"Output is: 4"
+"Output is: 5"
+☁️ "Running fiddle"
+"Welcome to Programiz!"
+"Output is: 0"
+"Output is: 1"
+"Output is: 2"
