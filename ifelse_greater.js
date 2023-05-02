a94a8fb0aef622c95924584c62f8fc2e7cf0af85
ChinthojouShivaKumar <124147339+ChinthojouShivaKumar@users.noreply.github.com>
2023-05-02 12:28:51 +0530

    Create ifelse_greater.js
    finding greater than and lesser than number

---
+++ ifelse_greater.js
@@ -0,0 +1,11 @@
+var a = prompt("enter the input");
+var num = 10;
+if (a>=num){
+    console.log("the given input is greater than or equal num")
+}
+else if (a<10) {
+    console.log("the given input is less than num")
+}
+else {
+alert("invalid input")
+}
