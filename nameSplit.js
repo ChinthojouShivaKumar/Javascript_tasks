95a04f1855f56808e1b4c2d1cd643ea17a216d65
ChinthojouShivaKumar <124147339+ChinthojouShivaKumar@users.noreply.github.com>
2023-05-05 07:38:33 +0530

    Create nameSplit.js
    spliting two combined words into two line of words

---
+++ nameSplit.js
@@ -0,0 +1,22 @@
+console.log("Welcome to Programiz!");
+
+
+let inputName = prompt("enter name")
+
+
+inputName_split1 = inputName.slice(0,6)
+alert(inputName_split1)
+
+inputName_split2 = inputName.slice(6,13)
+alert(inputName_split2)
+
+
+output:-
+  
+  
+node /tmp/8RWB6P6wR7.js
+Welcome to Programiz!
+enter name 10000 coders
+10000 coders
+10000 
+coders
